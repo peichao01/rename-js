3d51ce37c4d09e1194f73c13e1451e7da6a44add
peichao <380692952@qq.com>
2014-12-07 17:17:40 +0800

    增加文件模式，用来做一些高级处理

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ rename files/directories in the command line, use the javascript RegExp rules to
 在命令行批量重命名文件、目录，使用 javascript 的正则来匹配和替换文件名
 
 
-example:
+Install:
+----------
+```shell
+$ npm install rename-js -g
+```
+
+Usage:
 ---------
-directory public has some files: 
+directory `public` has some files: 
 
 1. index.js 
 2. index.css 
@@ -16,8 +22,9 @@ directory public has some files:
 , and i want to rename all files with `index` prefix to another name,
 then:
 
-```javascript
-node rename.js 'index\.(\w+)$' 'anotherName.$1'
+```shell
+$ cd /path/to/public
+$ rename 'index\.(\w+)$' 'anotherName.$1'
 ```
 
 the real script is:
@@ -31,3 +38,48 @@ then got:
 1. anotherName.js
 2. anotherName.css
 3. anotherName.html
+
+
+Advanced Usage
+----------------
+directory `public` has some files: 
+
+1. 4.jpg
+2. 10.jpg
+3. 99.jpg
+4. folder/45.jpg
+
+, and i want to rename all files to three-digits name, like `099.jpg`,
+then write a script named `processor.js`:
+
+processor.js
+```javascript
+function pad(num, len, _char){
+	if(len <= num.length) return num
+	return (new Array((len - num.length) + 1)).join(_char) + num
+}
+// type: 'file'|'directory'|'blockDevice'|'symbolicLink'|'characterDevice'|'FIFO'|'socket'
+// see `https://github.com/coolaj86/node-walk` for more info.
+module.exports = function(filename, type){
+    // if(type == 'file') xxx
+    // filename: folder/45.jpg
+    var m = filename.match(/^(folder\/)?(\d+)(\.jpg)$/)
+    if(m){
+        return (m[1] || '') + pad(m[2], 3, '0') + m[3]
+    }
+    // return the same string or undefined will not rename the file
+    return filename
+}
+```
+then run commands:
+
+```shell
+$cd /path/to/public
+$rename -f ./processor
+```
+last got:
+
+1. 004.jpg
+2. 010.jpg
+3. 099.jpg
+4. folder/045.jpg
--- rename.js
+++ rename.js
@@ -2,36 +2,61 @@ var walk = require('walk')
 var fs = require('fs')
 var _path = require('path')
 
+var fileMode, renameProcessorScript
+
 var from = process.argv[2]
 var to = process.argv[3]
 
 if(from === undefined || to === undefined){
-  console.log('[ERROR] arguments missed. see [https://github.com/peichao01/rename-js] for more infomation.')
-  return
+	console.log('[ERROR] arguments missed. see [https://github.com/peichao01/rename-js] for more infomation.')
+	return
 }
 
-var regFrom = new RegExp(from)
+if(from == '-f'){
+	fileMode = true
+	renameProcessorScript = to
+}
+else{
+	var regFrom = new RegExp(from)
 
 
-console.log('files need match RegExp: ' + regFrom)
+	console.log('files need match RegExp: ' + regFrom)
+}
 
 var walker = walk.walk(process.cwd(), {})
 
 walker.on('node', function(root, fileStats, next){
-  var filename = _path.join(root, fileStats.name)
-  if(filename.match(regFrom)){
-    //filename = _path.join(process.cwd(), filename)
-    var toName = filename.replace(regFrom, to)
-    console.log('renaming ['+filename+'] to ['+toName+'].')
-    fs.rename(filename, toName, function(err){
-      if(err) throw err
-      next()
-    })
-  }
-  else{
-    next()
-  }
+	var filename = _path.relative(process.cwd(), _path.join(root, fileStats.name))
+
+	if(fileMode){
+		var processor = require(_path.join(process.cwd(),  renameProcessorScript))
+		var toName = processor(filename)
+		if(typeof toName == 'string' && filename !== toName){
+			rename(filename, toName, next)
+		}
+		// 不处理此文件
+		else{
+			next()
+		}
+	}
+	else{
+		if(filename.match(regFrom)){
+			var toName = filename.replace(regFrom, to)
+			rename(filename, toName, next)
+		}
+		else{
+			next()
+		}
+	}
 })
 walker.on('end', function(){
-  console.log('rename done.')
+	console.log('rename done.')
 })
+
+function rename(filename, toName, next){
+	console.log('renaming ['+filename+'] to ['+toName+'].')
+	fs.rename(filename, toName, function(err){
+		if(err) throw err
+		next()
+	})
+}
